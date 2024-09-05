28544329ab79c97b74b0199e1afcce0fa77e08a5
malexj <alexmaximovj@gmail.com>
2024-09-05 13:52:16 +0300

    init

---
+++ src/test/java/com/malex/Application.java
@@ -0,0 +1,14 @@
+package com.malex;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class Application {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
